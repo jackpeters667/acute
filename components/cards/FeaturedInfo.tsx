136fa0781c1a883084c1db2f28288fe9beba45d0
rzcodes <rzcodes@gmail.com>
2021-10-29 14:53:42 +0200

    styles dashboard cards

--- components/cards/FeaturedInfo.tsx
+++ components/cards/FeaturedInfo.tsx
@@ -1,17 +1,24 @@
-export default function FeaturedInfo() {
+import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+type Entry = {
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  title: String;
+  number: String;
+};
+export default function FeaturedInfo(entry: Entry) {
   return (
     <div className="p-4 md:w-1/3">
       <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
         <div className="flex items-center mb-3">
-          <h2 className="text-white text-lg title-font font-medium">
-            Expenses
+          <h2 className="text-white text-2xl title-font font-medium">
+            {entry.title}
           </h2>
         </div>
         <div className="flex-grow">
-          <p className="leading-relaxed text-base">
-            Blue bottle crucifix vinyl post-ironic four dollar toast vegan
-            taxidermy. Gastropub indxgo juice poutine.
-          </p>
+          <div className="flex items-center text-white">
+            <entry.icon className="mr-1 text-xl" />
+            <p className="leading-relaxed text-xl">{entry.number}</p>
+          </div>
           <a className="mt-3 text-blue-400 inline-flex items-center">
             View More
             <svg
--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,3 +1,9 @@
+import {
+  MonetizationOn,
+  MonetizationOnTwoTone,
+  WatchLater,
+  Work,
+} from "@material-ui/icons";
 import React from "react";
 import FeaturedInfo from "../../components/cards/FeaturedInfo";
 
@@ -6,17 +12,21 @@ function dashboard() {
     <section className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-24 mx-auto flex flex-wrap">
         <div className="flex flex-col text-center w-full mb-20">
-          <h2 className="text-xs text-blue-400 tracking-widest font-medium title-font mb-1">
+          <h2 className="text-4xl text-blue-400 tracking-widest font-medium title-font mb-1">
             Welcome
           </h2>
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-white">
             Here are your overall statistics
           </h1>
         </div>
-        <div className="flex flex-wrap -m-4">
-          <FeaturedInfo />
-          <FeaturedInfo />
-          <FeaturedInfo />
+        <div className="flex flex-wrap -m-4 w-full">
+          <FeaturedInfo
+            icon={MonetizationOn}
+            title="Expenses"
+            number="R54783"
+          />
+          <FeaturedInfo icon={Work} title="Tasks" number="5" />
+          <FeaturedInfo icon={WatchLater} title="Hours" number="41" />
         </div>
       </div>
     </section>

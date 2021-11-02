f15d6d45758e955118269aeecc3a6619629814ef
rzcodes <rzcodes@gmail.com>
2021-11-02 11:46:29 +0200

    adds users from firebase

--- pages/admin/users/index.tsx
+++ pages/admin/users/index.tsx
@@ -3,8 +3,14 @@ import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import { DeleteOutline, ModeEdit } from "@mui/icons-material";
 import Link from "next/link";
 import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../../config/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function users() {
+  const [value, loading, error] = useCollection(collection(db, "users"), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
   const rows: GridRowsProp = [
     { id: 1, firstName: "Hello", lastName: "World" },
     { id: 2, firstName: "DataGridPro", lastName: "is Awesome" },
@@ -12,13 +18,17 @@ export default function users() {
   ];
 
   const columns: GridColDef[] = [
-    { field: "firstName", headerName: "First Name", width: 200 },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      width: 200,
+    },
     { field: "lastName", headerName: "Last Name", width: 200 },
     {
       field: "action",
       headerName: "Action",
       width: 150,
-      renderCell: (params) => {
+      renderCell: (params: { row: { id: any } }) => {
         return (
           <div className="cursor-pointer p-2">
             {/* change id number to be from array */}
@@ -39,25 +49,32 @@ export default function users() {
       },
     },
   ];
-  const [data, setData] = useState(rows);
 
   const handleDelete = (id: any) => {
-    try {
-      setData(data.filter((item) => item.id !== id));
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   setData(data.filter((item: { id: any; }) => item.id !== id));
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   return (
-    <div style={{ height: 300, width: "100%" }}>
-      <DataGrid
-        rows={data}
-        columns={columns}
-        checkboxSelection
-        disableSelectionOnClick
-        pageSize={8}
-      />
+    <div style={{ height: 800, width: "100%" }}>
+      {value && (
+        <DataGrid
+          rows={value.docs.map((row) => {
+            return {
+              id: row.id,
+              firstName: row.get("firstName"),
+              lastName: row.get("lastLastName"),
+            };
+          })}
+          columns={columns}
+          checkboxSelection
+          disableSelectionOnClick
+          pageSize={8}
+        />
+      )}
     </div>
   );
 }
--- pages/admin/users/new.tsx
+++ pages/admin/users/new.tsx
@@ -1,8 +1,37 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../../config/firebase";
+
 export default function New() {
+  const [fName, setFName] = useState("");
+  const [lName, setLname] = useState("");
+  const createUser = async (evt: { preventDefault: () => void }) => {
+    evt.preventDefault();
+    if (fName.length > 0 && lName.length > 0) {
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          firstName: fName,
+          lastName: lName,
+          created: serverTimestamp(),
+        });
+        console.log("Document written with ID: ", docRef.id);
+        setFName("");
+        setLname("");
+        alert("User created");
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    } else {
+      alert("Please check your entries");
+    }
+  };
   return (
     <div>
       <div className="newUserTitle">New User</div>
-      <form className="userUpdateForm flex justify-between mt-5">
+      <form
+        className="userUpdateForm flex justify-between mt-5"
+        onSubmit={createUser}
+      >
         <div className="userUpdateLeft">
           <div className="userUpdateItem flex flex-col mt-2">
             <label className="mb-1 text-sm">First Name</label>
@@ -10,6 +39,7 @@ export default function New() {
               type="text"
               className="userUpdateInput text-base shadow-sm w-60 border-none border-b-2 border-gray-600 h-7"
               placeholder="first name"
+              onChange={(e) => setFName(e.target.value)}
             />
           </div>
           <div className="userUpdateItem flex flex-col mt-2">
@@ -18,9 +48,13 @@ export default function New() {
               type="text"
               className="userUpdateInput shadow-sm w-60 text-base"
               placeholder="last name"
+              onChange={(e) => setLname(e.target.value)}
             />
           </div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            type="submit"
+          >
             Create
           </button>
         </div>

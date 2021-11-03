07b3381d797ae2594630fdd9552de273b1e155cb
rzcodes <rzcodes@gmail.com>
2021-11-03 11:35:47 +0200

    update imports

--- components/cards/FeaturedInfo.tsx
+++ components/cards/FeaturedInfo.tsx
@@ -1,5 +1,5 @@
-import { SvgIconTypeMap } from "@material-ui/core";
-import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 import Link from "next/link";
 type Entry = {
   icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;

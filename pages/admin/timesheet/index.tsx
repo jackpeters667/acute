8d290fa792a7e5e9da15e1d319adbf7381723ad3
rzcodes <rzcodes@gmail.com>
2021-11-03 07:57:03 +0200

    creates reusable function to format time

--- pages/admin/timesheet/index.tsx
+++ pages/admin/timesheet/index.tsx
@@ -19,6 +19,21 @@ const Timesheet: NextPage = () => {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
 
+  function getTime(params: GridValueFormatterParams) {
+    var date = new Date((params.value as number) * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime =
+      hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+    return `${formattedTime}`;
+  }
+
   const columns: GridColDef[] = [
     { field: "firstName", headerName: "First Name", width: 200 },
     { field: "lastName", headerName: "Last Name", width: 200 },
@@ -27,24 +42,14 @@ const Timesheet: NextPage = () => {
       field: "started",
       headerName: "Started",
       width: 200,
-      valueFormatter: (params: GridValueFormatterParams) => {
-        var date = new Date((params.value as number) * 1000);
-        // Hours part from the timestamp
-        var hours = date.getHours();
-        // Minutes part from the timestamp
-        var minutes = "0" + date.getMinutes();
-        // Seconds part from the timestamp
-        var seconds = "0" + date.getSeconds();
-
-        // Will display time in 10:30:23 format
-        var formattedTime =
-          hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-
-        console.log(formattedTime);
-        return `${formattedTime}`;
-      },
+      valueFormatter: getTime,
     },
-    { field: "ended", headerName: "Ended", width: 200 },
+    {
+      field: "ended",
+      headerName: "Ended",
+      width: 200,
+      valueFormatter: getTime,
+    },
     { field: "time", headerName: "Difference", width: 200 },
     {
       field: "action",

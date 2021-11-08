09aa90ba8f7390662bc688fa0b1f4e673570affe
rzcodes <rzcodes@gmail.com>
2021-11-08 11:09:57 +0200

    use material button

--- components/AddToTable.tsx
+++ components/AddToTable.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import Button from "@mui/material/Button";
+import { Add } from "@mui/icons-material";
 type Params = {
   path: string;
 };
@@ -7,9 +9,9 @@ type Params = {
 export default function AddToTable(path: Params) {
   return (
     <Link href={path.path}>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <Button variant="outlined" startIcon={<Add />}>
         New
-      </button>
+      </Button>
     </Link>
   );
 }
